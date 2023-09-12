59d2aa7a7d8cd2e5a9a66ce9df0590b0ce7e514b
ViniciusGoulart <viniciusbgoulart@hotmail.com>
2023-09-12 12:29:18 -0300

    Final version

--- src/main/java/org/fatec2023/Conversor.java
+++ src/main/java/org/fatec2023/Conversor.java
@@ -1,2 +1,75 @@
-package org.fatec2023;public class Conversor {
+package org.fatec2023;
+
+public class Conversor {
+
+    /**
+     * Converte um número decimal para outra base.
+     *
+     * @param base   A base para a qual converter o número.
+     * @param numero O número a ser convertido.
+     * @return A representação do número na base especificada.
+     */
+    public String decimalParaOutras(int base, String numero) {
+        try {
+            String resultado = "";
+            int num = Integer.parseInt(numero);
+
+            while (num != 0) {
+                String aux = Integer.toString(num % base);
+                resultado = numeroParaHexadecimal(aux) + resultado;
+                num = num / base;
+            }
+
+            return resultado;
+        } catch (NumberFormatException ex) {
+            return "Este valor não é da base decimal: " + numero;
+        }
+    }
+
+    /**
+     * Converte um número de outra base para decimal.
+     *
+     * @param base   A base do número de entrada.
+     * @param numero O número a ser convertido.
+     * @return A representação decimal do número de entrada.
+     */
+    public String outrasParaDecimal(int base, String numero) {
+        int n = numero.length();
+        int expoente = 0;
+        double resultado = 0;
+
+        for (int i = n; i > 0; i--) {
+            char aux = numero.charAt(i - 1);
+            resultado += hexadecimalParaNumero(aux) * Math.pow(base, expoente);
+            expoente++;
+        }
+
+        return Integer.toString((int) resultado);
+    }
+    String numeroParaHexadecimal(String aux) {
+
+        switch (aux) {
+            case "10": return "A";
+            case "11": return "B";
+            case "12": return "C";
+            case "13": return "D";
+            case "14": return "E";
+            case "15": return "F";
+            default:   return aux;
+        }
+    }
+
+    int hexadecimalParaNumero(char c) {
+
+        if (c >= '0' && c <= '9') {
+            return c - '0'; // Transforma os caracteres adquiridos da string transformando eles em numeros
+        } else if (c >= 'A' && c <= 'F') {
+            return c - 'A' + 10; // Caso seja uma letra do alfabeto ele ira transformar em numero acrescentado 10
+        }
+
+        // Caso ocorra algum erro ele retornara -1
+        return -1;
+
+    }
+
 }
--- src/main/java/org/fatec2023/Main.java
+++ src/main/java/org/fatec2023/Main.java
@@ -1,7 +1,81 @@
-package org.fatec2023-2;
+package org.fatec2023;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        Conversor conv = new Conversor();
+
+        int base1, base2,fim = 0;
+        String resultado = "",numero = "";
+
+
+        do {
+            try {
+                System.out.print("Digite a base do número a ser convertido (2 a 16): ");
+                base1 = sc.nextInt();
+
+                if (base1 < 2 || base1 > 16) {
+                    System.out.println("Base inválida. Tente novamente.\n");
+                    continue;
+                }
+
+                System.out.print("Digite o número a ser convertido (na base " + base1 + "): ");
+                numero = sc.next().toUpperCase();
+
+                if (!validarNumero(numero, base1)) {
+                    System.out.println("Número inválido para a base " + base1 + ". Tente novamente.\n");
+                    continue;
+                }
+
+                System.out.print("Digite a base desejada (2 a 16): ");
+                base2 = sc.nextInt();
+
+                if (base2 < 2 || base2 > 16) {
+                    System.out.println("Base inválida. Tente novamente.\n");
+                    continue;
+                }
+
+                if (base1 == 10) {
+                    resultado = conv.decimalParaOutras(base2, numero);
+
+                    System.out.println("\nO número " + numero + " convertido para a base (" + base2 + ") é: " + resultado);
+                } else {
+                    String aux = conv.outrasParaDecimal(base1, numero);
+                    resultado = conv.decimalParaOutras(base2, aux);
+
+                    System.out.println("\nO número " + numero + " convertido para a base (" + base2 + ") é: " + resultado);
+                }
+
+
+
+                System.out.println("\nDeseja fazer outra conversão?\n 1 - SIM  2 - NÃO");
+                fim = sc.nextInt();
+            }catch (InputMismatchException exception){
+                System.out.println("Erro ao atribuir valor as bases, por favor digite apenas numeros");
+                fim = 2;
+            }
+
+        } while (fim != 2);
+    }
+
+    // Verifica se o número fornecido é válido para a base especificada
+    private static boolean validarNumero(String numero, int base) {
+        for (char c : numero.toCharArray()) {
+            if (!isCaracterValido(c, base)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    // Verifica se um caractere é válido para a base especificada
+    private static boolean isCaracterValido(char c, int base) {
+        if (base <= 10) {
+            return c >= '0' && c < ('0' + base);
+        } else {
+            return (c >= '0' && c <= '9') || (c >= 'A' && c < ('A' + base - 10));
+        }
     }
 }
